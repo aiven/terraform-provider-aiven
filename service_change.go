7a5be5cb7f55a44c00372fcc6e63ce8dc6da5971
Jelmer Snoeck <jelmer@siphoc.com>
2018-07-18 05:36:11 -0700

    Fix up linting issues.

--- service_change.go
+++ service_change.go
@@ -1,18 +1,22 @@
 package main
 
 import (
-	"github.com/hashicorp/terraform/helper/resource"
-	"github.com/jelmersnoeck/aiven"
 	"log"
 	"time"
+
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/jelmersnoeck/aiven"
 )
 
+// ServiceChangeWaiter is used to refresh the Aiven Service endpoints when
+// provisioning.
 type ServiceChangeWaiter struct {
 	Client      *aiven.Client
 	Project     string
 	ServiceName string
 }
 
+// RefreshFunc will call the Aiven client and refresh it's state.
 func (w *ServiceChangeWaiter) RefreshFunc() resource.StateRefreshFunc {
 	return func() (interface{}, string, error) {
 		service, err := w.Client.Services.Get(
@@ -30,6 +34,7 @@ func (w *ServiceChangeWaiter) RefreshFunc() resource.StateRefreshFunc {
 	}
 }
 
+// Conf sets up the configuration to refresh.
 func (w *ServiceChangeWaiter) Conf() *resource.StateChangeConf {
 	state := &resource.StateChangeConf{
 		Pending: []string{"REBUILDING"},
@@ -39,6 +44,6 @@ func (w *ServiceChangeWaiter) Conf() *resource.StateChangeConf {
 	state.Delay = 10 * time.Second
 	state.Timeout = 10 * time.Minute
 	state.MinTimeout = 2 * time.Second
-	return state
 
+	return state
 }
