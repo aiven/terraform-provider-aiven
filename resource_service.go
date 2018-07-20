7297481fdef0a66c8488d21adc469c3d01cc3c3a
Benjamin Goldberg <ben@spothero.com>
2018-07-20 12:02:21 -0500

    Add support for User Config and Kafka User Config checking.

--- resource_service.go
+++ resource_service.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/jelmersnoeck/aiven"
@@ -14,12 +15,11 @@ func resourceService() *schema.Resource {
 		Update: resourceServiceUpdate,
 		Delete: resourceServiceDelete,
 
-		// TODO: add user config
 		Schema: map[string]*schema.Schema{
 			"project": {
 				Type:        schema.TypeString,
 				Required:    true,
-				Description: "Target cloud",
+				Description: "Target project",
 				ForceNew:    true,
 			},
 			"cloud": {
@@ -64,13 +64,17 @@ func resourceService() *schema.Resource {
 				Computed:    true,
 				Description: "Service state",
 			},
+			"user_config": {
+				Type:        schema.TypeMap,
+				Optional:    true,
+				Description: "Service type-specific settings",
+			},
 		},
 	}
 }
 
 func resourceServiceCreate(d *schema.ResourceData, m interface{}) error {
 	client := m.(*aiven.Client)
-
 	service, err := client.Services.Create(
 		d.Get("project").(string),
 		aiven.CreateServiceRequest{
@@ -79,6 +83,7 @@ func resourceServiceCreate(d *schema.ResourceData, m interface{}) error {
 			Plan:        d.Get("plan").(string),
 			ServiceName: d.Get("service_name").(string),
 			ServiceType: d.Get("service_type").(string),
+			UserConfig:  transformUserConfig(d),
 		},
 	)
 
@@ -136,10 +141,11 @@ func resourceServiceUpdate(d *schema.ResourceData, m interface{}) error {
 		d.Get("project").(string),
 		d.Get("service_name").(string),
 		aiven.UpdateServiceRequest{
-			Cloud:     d.Get("cloud").(string),
-			GroupName: d.Get("group_name").(string),
-			Plan:      d.Get("plan").(string),
-			Powered:   true,
+			Cloud:      d.Get("cloud").(string),
+			GroupName:  d.Get("group_name").(string),
+			Plan:       d.Get("plan").(string),
+			Powered:    true,
+			UserConfig: transformUserConfig(d),
 		},
 	)
 	if err != nil {
@@ -177,4 +183,42 @@ func resourceServiceWait(d *schema.ResourceData, m interface{}) error {
 	}
 
 	return nil
+}
+
+// Aiven requires field types on received JSON to be correctly typed. If the service type is known
+// transform the Terraform type (one of string, list, or map) into the appropriate Go type and
+// return the modified user config. If the  service does not have a special handler the user config
+// is returned as-is with the default Terraform types associated.
+func transformUserConfig(d *schema.ResourceData) map[string]interface{} {
+	serviceType := d.Get("service_type").(string)
+	userConfig := d.Get("user_config").(map[string]interface{})
+
+	if serviceType == "kafka" {
+		userConfig = transformKafkaUserConfig(userConfig)
+	}
+
+	return userConfig
+}
+
+// Transform the kafka service user config from Terraform's built-in types to the appropriate
+// golang types.
+func transformKafkaUserConfig(userConfig map[string]interface{}) map[string]interface{} {
+	newUserConfig := make(map[string]interface{})
+	kafkaConnectInterface, ok := userConfig["kafka_connect"]
+	if ok {
+		newUserConfig["kafka_connect"] = stringToBool(kafkaConnectInterface.(string))
+	}
+	kafkaRestInterface, ok := userConfig["kafka_rest"]
+	if ok {
+		newUserConfig["kafka_rest"] = stringToBool(kafkaRestInterface.(string))
+	}
+	schemaRegistryInterface, ok := userConfig["schema_registry"]
+	if ok {
+		newUserConfig["schema_registry"] = stringToBool(schemaRegistryInterface.(string))
+	}
+	return newUserConfig
+}
+
+func stringToBool(maybeBool string) bool {
+	return maybeBool == "1" || strings.ToLower(maybeBool) == "true"
 }
