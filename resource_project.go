824169e413901dc91a347d2c80b653d9140938f7
Rauli Ikonen <rauli@aiven.io>
2018-09-25 12:26:28 +0300

    Allow managing project billing info & tech emails

--- resource_project.go
+++ resource_project.go
@@ -19,6 +19,17 @@ func resourceProject() *schema.Resource {
 		},
 
 		Schema: map[string]*schema.Schema{
+			"billing_address": {
+				Type:        schema.TypeString,
+				Description: "Billing name and address of the project",
+				Optional:    true,
+			},
+			"billing_emails": {
+				Type:        schema.TypeSet,
+				Description: "Billing contact emails of the project",
+				Elem:        &schema.Schema{Type: schema.TypeString},
+				Optional:    true,
+			},
 			"ca_cert": {
 				Type:        schema.TypeString,
 				Computed:    true,
@@ -36,10 +47,21 @@ func resourceProject() *schema.Resource {
 				Description:      "Copy properties from another project. Only has effect when a new project is created.",
 				DiffSuppressFunc: createOnlyDiffSuppressFunc,
 			},
+			"country_code": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Billing country code of the project",
+			},
 			"project": {
 				Type:        schema.TypeString,
 				Required:    true,
 				Description: "Project name",
+			},
+			"technical_emails": {
+				Type:        schema.TypeSet,
+				Description: "Technical contact emails of the project",
+				Elem:        &schema.Schema{Type: schema.TypeString},
+				Optional:    true,
 			},
 		},
 	}
@@ -54,9 +76,13 @@ func resourceProjectCreate(d *schema.ResourceData, m interface{}) error {
 	projectName := d.Get("project").(string)
 	project, err := client.Projects.Create(
 		aiven.CreateProjectRequest{
+			BillingAddress:  optionalStringPointer(d, "billing_address"),
+			BillingEmails:   contactEmailListForAPI(d, "billing_emails", true),
 			CardID:          cardID,
 			CopyFromProject: d.Get("copy_from_project").(string),
+			CountryCode:     optionalStringPointer(d, "country_code"),
 			Project:         projectName,
+			TechnicalEmails: contactEmailListForAPI(d, "technical_emails", true),
 		},
 	)
 	if err != nil {
@@ -76,13 +102,12 @@ func resourceProjectRead(d *schema.ResourceData, m interface{}) error {
 		return err
 	}
 
-	d.Set("project", project.Name)
 	// Don't set card id unconditionally to prevent converting short card id format to long
 	currentCardID, err := getLongCardID(client, d.Get("card_id").(string))
 	if err != nil || currentCardID != project.Card.CardID {
 		d.Set("card_id", project.Card.CardID)
 	}
-	resourceProjectGetCACert(project.Name, client, d)
+	setProjectTerraformProperties(d, client, project)
 	return nil
 }
 
@@ -93,10 +118,16 @@ func resourceProjectUpdate(d *schema.ResourceData, m interface{}) error {
 	if err != nil {
 		return err
 	}
+	billingAddress := d.Get("billing_address").(string)
+	countryCode := d.Get("country_code").(string)
 	project, err := client.Projects.Update(
 		d.Get("project").(string),
 		aiven.UpdateProjectRequest{
-			CardID: cardID,
+			BillingAddress:  &billingAddress,
+			BillingEmails:   contactEmailListForAPI(d, "billing_emails", false),
+			CardID:          cardID,
+			CountryCode:     &countryCode,
+			TechnicalEmails: contactEmailListForAPI(d, "technical_emails", false),
 		},
 	)
 	if err != nil {
@@ -128,9 +159,8 @@ func resourceProjectState(d *schema.ResourceData, m interface{}) ([]*schema.Reso
 		return nil, err
 	}
 
-	d.Set("project", project.Name)
 	d.Set("card_id", project.Card.CardID)
-	resourceProjectGetCACert(project.Name, client, d)
+	setProjectTerraformProperties(d, client, project)
 
 	return []*schema.ResourceData{d}, nil
 }
@@ -151,4 +181,49 @@ func getLongCardID(client *aiven.Client, cardID string) (string, error) {
 		return card.CardID, nil
 	}
 	return cardID, nil
+}
+
+func contactEmailListForAPI(d *schema.ResourceData, field string, newResource bool) *[]*aiven.ContactEmail {
+	var results []*aiven.ContactEmail
+	// We don't want to send empty list for new resource if data is copied from other
+	// project to prevent accidental override of the emails being copied. Empty array
+	// should be sent if user has explicitly defined that even when copy_from_project
+	// is set but Terraform does not support checking that; d.GetOkExists returns false
+	// even if the value is set (to empty).
+	if len(d.Get("copy_from_project").(string)) == 0 || !newResource {
+		results = []*aiven.ContactEmail{}
+	}
+	valuesInterface, ok := d.GetOk(field)
+	if ok && valuesInterface != nil {
+		for _, emailInterface := range valuesInterface.(*schema.Set).List() {
+			results = append(results, &aiven.ContactEmail{Email: emailInterface.(string)})
+		}
+	}
+	if results == nil {
+		return nil
+	}
+	return &results
+}
+
+func contactEmailListForTerraform(d *schema.ResourceData, field string, contactEmails []*aiven.ContactEmail) {
+	_, existsBefore := d.GetOk(field)
+	if !existsBefore && (contactEmails == nil || len(contactEmails) == 0) {
+		return
+	}
+	results := []string{}
+	if contactEmails != nil {
+		for _, contactEmail := range contactEmails {
+			results = append(results, contactEmail.Email)
+		}
+	}
+	d.Set(field, results)
+}
+
+func setProjectTerraformProperties(d *schema.ResourceData, client *aiven.Client, project *aiven.Project) {
+	d.Set("billing_address", project.BillingAddress)
+	contactEmailListForTerraform(d, "billing_emails", project.BillingEmails)
+	d.Set("country_code", project.CountryCode)
+	d.Set("project", project.Name)
+	contactEmailListForTerraform(d, "technical_emails", project.TechnicalEmails)
+	resourceProjectGetCACert(project.Name, client, d)
 }
