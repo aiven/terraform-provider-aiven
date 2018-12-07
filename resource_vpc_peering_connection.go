ec7a1b439d97b6ec7bd505ff68fc69705cdf7d2e
Rauli Ikonen <rauli@aiven.io>
2018-12-07 13:17:12 +0200

    Support VPC peering connection ID

--- resource_vpc_peering_connection.go
+++ resource_vpc_peering_connection.go
@@ -3,9 +3,12 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"strings"
+	"time"
 
 	"github.com/aiven/aiven-go-client"
+	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/helper/schema"
 )
 
@@ -43,6 +46,11 @@ func resourceVPCPeeringConnection() *schema.Resource {
 				Description: "State of the peering connection",
 				Type:        schema.TypeString,
 			},
+			"peering_connection_id": {
+				Computed:    true,
+				Description: "Cloud provider identifier for the peering connection if available",
+				Type:        schema.TypeString,
+			},
 		},
 	}
 }
@@ -63,6 +71,20 @@ func resourceVPCPeeringConnectionCreate(d *schema.ResourceData, m interface{}) e
 		return err
 	}
 
+	// Wait until the peering connection has actually been built
+	w := &VPCPeeringBuildWaiter{
+		Client:           m.(*aiven.Client),
+		Project:          projectName,
+		VPCID:            vpcID,
+		PeerCloudAccount: pc.PeerCloudAccount,
+		PeerVPC:          pc.PeerVPC,
+	}
+	res, err := w.Conf().WaitForState()
+	if err != nil {
+		return err
+	}
+
+	pc = res.(*aiven.VPCPeeringConnection)
 	d.SetId(buildResourceID(projectName, vpcID, pc.PeerCloudAccount, pc.PeerVPC))
 	return copyVPCPeeringConnectionPropertiesFromAPIResponseToTerraform(d, pc, projectName, vpcID)
 }
@@ -117,6 +139,58 @@ func copyVPCPeeringConnectionPropertiesFromAPIResponseToTerraform(
 	d.Set("peer_cloud_account", peeringConnection.PeerCloudAccount)
 	d.Set("peer_vpc", peeringConnection.PeerVPC)
 	d.Set("state", peeringConnection.State)
+	if peeringConnection.StateInfo != nil {
+		peeringID, ok := (*peeringConnection.StateInfo)["aws_vpc_peering_connection_id"]
+		if ok {
+			d.Set("peering_connection_id", peeringID)
+		}
+	}
 
 	return nil
+}
+
+// VPCPeeringBuildWaiter is used to wait for Aiven to build a new VPC peering connection
+// so that ID becomes available (when applicable)
+type VPCPeeringBuildWaiter struct {
+	Client           *aiven.Client
+	Project          string
+	VPCID            string
+	PeerCloudAccount string
+	PeerVPC          string
+}
+
+// RefreshFunc will call the Aiven client and refresh it's state.
+func (w *VPCPeeringBuildWaiter) RefreshFunc() resource.StateRefreshFunc {
+	return func() (interface{}, string, error) {
+		pc, err := w.Client.VPCPeeringConnections.Get(w.Project, w.VPCID, w.PeerCloudAccount, w.PeerVPC)
+
+		if err != nil {
+			return nil, "", err
+		}
+
+		log.Printf("[DEBUG] Got %s state while waiting for peering connection to be built.", pc.State)
+
+		return pc, pc.State, nil
+	}
+}
+
+// Conf sets up the configuration to refresh.
+func (w *VPCPeeringBuildWaiter) Conf() *resource.StateChangeConf {
+	state := &resource.StateChangeConf{
+		Pending: []string{"APPROVED"},
+		Target: []string{
+			"ACTIVE",
+			"REJECTED_BY_PEER",
+			"PENDING_PEER",
+			"INVALID_SPECIFICATION",
+			"DELETING",
+			"DELETED",
+			"DELETED_BY_PEER",
+		},
+		Refresh: w.RefreshFunc(),
+	}
+	state.Delay = 10 * time.Second
+	state.Timeout = 2 * time.Minute
+	state.MinTimeout = 2 * time.Second
+	return state
 }
