560b0f2fb5941de7b1e86f52c9522648b4924273
Marko Teiste <mte@aiven.io>
2019-09-02 08:25:25 +0300

    fix schema template location

--- generate_templates.sh
+++ generate_templates.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
-avn service types -v --json | jq '[to_entries[] | {"key": .key, "value": .value.user_config_schema}] | from_entries' > templates/service_user_config_schema.json
-avn service integration-endpoint-types-list --project test --json | jq 'map({(.endpoint_type): .user_config_schema}) | add' > templates/integration_endpoints_user_config_schema.json
-avn service integration-types-list --project test --json | jq 'map({(.integration_type): .user_config_schema}) | add' > templates/integrations_user_config_schema.json
+avn service types -v --json | jq '[to_entries[] | {"key": .key, "value": .value.user_config_schema}] | from_entries' > aiven/templates/service_user_config_schema.json
+avn service integration-endpoint-types-list --project test --json | jq 'map({(.endpoint_type): .user_config_schema}) | add' > aiven/templates/integration_endpoints_user_config_schema.json
+avn service integration-types-list --project test --json | jq 'map({(.integration_type): .user_config_schema}) | add' > aiven/templates/integrations_user_config_schema.json
