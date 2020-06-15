e5fa0ebad49fca4acb7be461bbe9acf57aeff7d0
Ivan Savciuc <savciuci@aiven.io>
2020-06-15 21:54:31 +0200

    add kafka_connect tf resource

---
+++ aiven/resource_kafka_connect.go
@@ -0,0 +1,212 @@
+package aiven
+
+import (
+	"github.com/hashicorp/terraform/helper/schema"
+	"time"
+)
+
+var aivenKafkaConnectSchema = map[string]*schema.Schema{
+	"project": {
+		Type:        schema.TypeString,
+		Required:    true,
+		Description: "Target project",
+		ForceNew:    true,
+	},
+	"cloud_name": {
+		Type:        schema.TypeString,
+		Optional:    true,
+		Description: "Cloud the service runs in",
+	},
+	"plan": {
+		Type:        schema.TypeString,
+		Optional:    true,
+		Description: "Subscription plan",
+	},
+	"service_name": {
+		Type:        schema.TypeString,
+		Required:    true,
+		Description: "Service name",
+		ForceNew:    true,
+	},
+	"service_type": {
+		Type:        schema.TypeString,
+		Computed:    true,
+		Description: "Aiven internal service type code",
+	},
+	"project_vpc_id": {
+		Type:        schema.TypeString,
+		Optional:    true,
+		Description: "Identifier of the VPC the service should be in, if any",
+	},
+	"maintenance_window_dow": {
+		Type:        schema.TypeString,
+		Optional:    true,
+		Description: "Day of week when maintenance operations should be performed. One monday, tuesday, wednesday, etc.",
+		DiffSuppressFunc: func(k, old, new string, d *schema.ResourceData) bool {
+			return new == ""
+		},
+	},
+	"maintenance_window_time": {
+		Type:        schema.TypeString,
+		Optional:    true,
+		Description: "Time of day when maintenance operations should be performed. UTC time in HH:mm:ss format.",
+		DiffSuppressFunc: func(k, old, new string, d *schema.ResourceData) bool {
+			return new == ""
+		},
+	},
+	"termination_protection": {
+		Type:        schema.TypeBool,
+		Optional:    true,
+		Description: "Prevent service from being deleted. It is recommended to have this enabled for all services.",
+	},
+	"service_uri": {
+		Type:        schema.TypeString,
+		Computed:    true,
+		Description: "URI for connecting to the service. Service specific info is under \"kafka\", \"pg\", etc.",
+		Sensitive:   true,
+	},
+	"service_host": {
+		Type:        schema.TypeString,
+		Computed:    true,
+		Description: "Service hostname",
+	},
+	"service_port": {
+		Type:        schema.TypeInt,
+		Computed:    true,
+		Description: "Service port",
+	},
+	"service_password": {
+		Type:        schema.TypeString,
+		Computed:    true,
+		Description: "Password used for connecting to the service, if applicable",
+		Sensitive:   true,
+	},
+	"service_username": {
+		Type:        schema.TypeString,
+		Computed:    true,
+		Description: "Username used for connecting to the service, if applicable",
+	},
+	"state": {
+		Type:        schema.TypeString,
+		Computed:    true,
+		Description: "Service state",
+	},
+	"service_integrations": {
+		Type:             schema.TypeList,
+		Optional:         true,
+		Description:      "Service integrations to specify when creating a service. Not applied after initial service creation",
+		DiffSuppressFunc: createOnlyDiffSuppressFunc,
+		Elem: &schema.Resource{
+			Schema: map[string]*schema.Schema{
+				"source_service_name": {
+					Type:        schema.TypeString,
+					Required:    true,
+					Description: "Name of the source service",
+				},
+				"integration_type": {
+					Type:        schema.TypeString,
+					Required:    true,
+					Description: "Type of the service integration. The only supported value at the moment is 'read_replica'",
+				},
+			},
+		},
+	},
+	"components": {
+		Type:        schema.TypeList,
+		Computed:    true,
+		Description: "Service component information objects",
+		Elem: &schema.Resource{
+			Schema: map[string]*schema.Schema{
+				"component": {
+					Type:        schema.TypeString,
+					Computed:    true,
+					Description: "Service component name",
+				},
+				"host": {
+					Type:        schema.TypeString,
+					Computed:    true,
+					Description: "DNS name for connecting to the service component",
+				},
+				"kafka_authentication_method": {
+					Type:        schema.TypeString,
+					Computed:    true,
+					Optional:    true,
+					Description: "Kafka authentication method. This is a value specific to the 'kafka' service component",
+				},
+				"port": {
+					Type:        schema.TypeInt,
+					Computed:    true,
+					Description: "Port number for connecting to the service component",
+				},
+				"route": {
+					Type:        schema.TypeString,
+					Computed:    true,
+					Description: "Network access route",
+				},
+				"ssl": {
+					Type:     schema.TypeBool,
+					Computed: true,
+					Description: "Whether the endpoint is encrypted or accepts plaintext. By default endpoints are " +
+						"always encrypted and this property is only included for service components they may " +
+						"disable encryption",
+				},
+				"usage": {
+					Type:        schema.TypeString,
+					Computed:    true,
+					Description: "DNS usage name",
+				},
+			},
+		},
+	},
+	"kafka_connect_user_config": {
+		Type:             schema.TypeList,
+		MaxItems:         1,
+		Optional:         true,
+		Description:      "Kafka specific user configurable settings",
+		DiffSuppressFunc: emptyObjectDiffSuppressFunc,
+		Elem: &schema.Resource{
+			Schema: GenerateTerraformUserConfigSchema(
+				GetUserConfigSchema("service")[ServiceTypeKafkaConnect].(map[string]interface{})),
+		},
+	},
+	"kafka_connect": {
+		Type:        schema.TypeList,
+		MaxItems:    1,
+		Computed:    true,
+		Description: "Kafka Connect specific server provided values",
+		Optional:    true,
+		Elem: &schema.Resource{
+			Schema: map[string]*schema.Schema{},
+		},
+	},
+}
+
+func resourceKafkaConnect() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceKafkaConnectCreate,
+		Read:   resourceServiceRead,
+		Update: resourceServiceUpdate,
+		Delete: resourceServiceDelete,
+		Exists: resourceServiceExists,
+		Importer: &schema.ResourceImporter{
+			State: resourceServiceState,
+		},
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(20 * time.Minute),
+			Update: schema.DefaultTimeout(20 * time.Minute),
+		},
+
+		Schema: aivenKafkaConnectSchema,
+	}
+}
+
+func resourceKafkaConnectCreate(d *schema.ResourceData, m interface{}) error {
+	if err := d.Set("service_type", ServiceTypeKafkaConnect); err != nil {
+		return err
+	}
+	if err := d.Set("kafka_connect", []map[string]interface{}{}); err != nil {
+		return err
+	}
+
+	return resourceServiceCreate(d, m)
+}
