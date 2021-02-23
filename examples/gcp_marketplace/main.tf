a034e191bd2b651aeccfc96c9caf46caa2ca716e
Charlie Hornsby <chornsby@aiven.io>
2021-02-23 15:18:31 +0200

    Add example initial configuration for GCP Marketplace

---
+++ examples/gcp_marketplace/main.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_providers {
+    aiven = {
+      source  = "aiven/aiven"
+      version = "2.X.X"
+    }
+  }
+}
+
+provider "aiven" {
+  api_token = var.aiven_api_token
+}
+
+# Creating a service in an existing project
+
+data "aiven_project" "existing_project" {
+  project = "existing_project"
+}
+
+resource "aiven_kafka" "kafka" {
+  project      = data.aiven_project.existing_project.id
+  cloud_name   = "google-europe-north1"
+  plan         = "gcp-marketplace-startup-2"
+  service_name = "kafka"
+}
+
+# Creating a service in a new project
+
+data "aiven_account" "test_account" {
+  name = var.aiven_account_name
+}
+
+resource "aiven_project" "new_project" {
+  project    = "new_project"
+  account_id = data.aiven_account.test_account.id # This is required for new marketplace projects
+}
+
+resource "aiven_elasticsearch" "elasticsearch" {
+  project      = data.aiven_project.existing_project.id
+  cloud_name   = "google-europe-north1"
+  plan         = "gcp-marketplace-startup-4"
+  service_name = "elasticsearch"
+}
