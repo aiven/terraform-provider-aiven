65f1c4547e8bf93d8316b214d9d1f88c233f21a9
Ivan Savciuc <ivan.savciuc@gmail.com>
2021-03-05 12:12:08 +0200

    use latest go-client

--- examples/aws_privatelink/provider.tf
+++ examples/aws_privatelink/provider.tf
@@ -1,16 +1,14 @@
 terraform {
   required_providers {
     aiven = {
-      //      source = "aiven/aiven"
-      //      version = "2.X.X"
-      source = "aiven.io/provider/aiven"
-      version = "2.0.0"
+            source = "aiven/aiven"
+            version = "2.X.X"
     }
   }
 }
 
 # Initialize provider. No other config options than api_token
 provider "aiven" {
-  //  api_token = var.aiven_api_token
+    api_token = var.aiven_api_token
 }
 
