75185f7374508dd3808d55e60658bf28208a65db
Don Dang <don.dang@aiven.io>
2022-06-16 18:18:38 +0300

    Allow retrieving project vpc datasource by id
    In case there are multiple VPCs returned, user now can filter
    and get a specific VPC by providing the id.
    
    [DARK-628]

--- examples/data-sources/aiven_project_vpc/data-source.tf
+++ examples/data-sources/aiven_project_vpc/data-source.tf
@@ -3,3 +3,9 @@ data "aiven_project_vpc" "myvpc" {
   cloud_name = "google-europe-west1"
 }
 
+# Or
+data "aiven_project_vpc" "myvpc" {
+  project    = aiven_project.myproject.project
+  cloud_name = "google-europe-west1"
+  id         = aiven_project_vpc.vpc.id
+}
