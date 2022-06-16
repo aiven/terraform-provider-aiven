c647706fc58ce8733657960c7657e369ee7ff72a
Don Dang <don.dang@aiven.io>
2022-06-16 11:35:24 +0300

    Support retrieving multiple project vpcs datasource
    Some users might have more than one vpcs per cloud, and we allows
    showing all the retrieved data in the new `vpcs` attribute of the
    datasource schema.

--- internal/service/vpc/datasource_project_vpc.go
+++ internal/service/vpc/datasource_project_vpc.go
@@ -2,6 +2,8 @@ package vpc
 
 import (
 	"context"
+	"strconv"
+	"time"
 
 	"github.com/aiven/aiven-go-client"
 	"github.com/aiven/terraform-provider-aiven/internal/schemautil"
@@ -11,11 +13,39 @@ import (
 )
 
 func DatasourceProjectVPC() *schema.Resource {
+	vpc_schema := schemautil.ResourceSchemaAsDatasourceSchema(
+		aivenProjectVPCSchema,
+		"project",
+		"cloud_name",
+	)
+	vpc_schema["vpcs"] = &schema.Schema{
+		Type:        schema.TypeList,
+		Computed:    true,
+		Description: "VPCs",
+		Elem: &schema.Resource{
+			Schema: map[string]*schema.Schema{
+				"network_cidr": {
+					Required:    true,
+					ForceNew:    true,
+					Type:        schema.TypeString,
+					Description: "Network address range used by the VPC like 192.168.0.0/24",
+				},
+				"state": {
+					Computed:    true,
+					Type:        schema.TypeString,
+					Description: schemautil.Complex("State of the VPC.").PossibleValues("APPROVED", "ACTIVE", "DELETING", "DELETED").Build(),
+				},
+				"id": {
+					Computed: true,
+					Type:     schema.TypeString,
+				},
+			},
+		},
+	}
 	return &schema.Resource{
 		ReadContext: datasourceProjectVPCRead,
 		Description: "The Project VPC data source provides information about the existing Aiven Project VPC.",
-		Schema: schemautil.ResourceSchemaAsDatasourceSchema(aivenProjectVPCSchema,
-			"project", "cloud_name"),
+		Schema:      vpc_schema,
 	}
 }
 
@@ -25,23 +55,58 @@ func datasourceProjectVPCRead(_ context.Context, d *schema.ResourceData, m inter
 	projectName := d.Get("project").(string)
 	cloudName := d.Get("cloud_name").(string)
 
+	vpcs_list := make([]map[string]string, 0)
+
 	vpcs, err := client.VPCs.List(projectName)
 	if err != nil {
 		return diag.FromErr(err)
 	}
 
+	filtered_vpcs := make([]*aiven.VPC, 0)
 	for _, vpc := range vpcs {
 		if vpc.CloudName == cloudName {
-			d.SetId(schemautil.BuildResourceID(projectName, vpc.ProjectVPCID))
-			err = copyVPCPropertiesFromAPIResponseToTerraform(d, vpc, projectName)
-			if err != nil {
-				return diag.FromErr(err)
-			}
-
-			return nil
+			filtered_vpcs = append(filtered_vpcs, vpc)
 		}
 	}
 
-	return diag.Errorf("project %s has no VPC defined for %s",
-		projectName, cloudName)
+	if len(filtered_vpcs) == 0 {
+		return diag.Errorf("project %s has no VPC defined for %s",
+			projectName, cloudName)
+	}
+
+	if len(filtered_vpcs) == 1 {
+		vpc := filtered_vpcs[0]
+		d.SetId(schemautil.BuildResourceID(projectName, vpc.ProjectVPCID))
+		err = copyVPCPropertiesFromAPIResponseToTerraform(d, vpc, projectName)
+		if err != nil {
+			return diag.FromErr(err)
+		}
+		return nil
+	}
+
+	for _, vpc := range filtered_vpcs {
+		data := map[string]string{
+			"network_cidr": vpc.NetworkCIDR,
+			"state":        vpc.State,
+			"id":           schemautil.BuildResourceID(projectName, vpc.ProjectVPCID),
+		}
+
+		vpcs_list = append(vpcs_list, data)
+	}
+
+	d.SetId(strconv.FormatInt(time.Now().Unix(), 10))
+
+	if err := d.Set("project", projectName); err != nil {
+		return diag.FromErr(err)
+	}
+
+	if err := d.Set("cloud_name", cloudName); err != nil {
+		return diag.FromErr(err)
+	}
+
+	if err := d.Set("vpcs", vpcs_list); err != nil {
+		return diag.FromErr(err)
+	}
+
+	return nil
 }
