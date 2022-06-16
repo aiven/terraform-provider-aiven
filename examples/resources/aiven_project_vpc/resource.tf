ea59b05c44255018a2514bc79dacf88ddf00d060
Juho Heikkinen <heikju@aiven.io>
2022-06-16 17:30:21 +0300

    Fix invalid network CIDR in Aiven VPC resource example (#758)

--- examples/resources/aiven_project_vpc/resource.tf
+++ examples/resources/aiven_project_vpc/resource.tf
@@ -1,7 +1,7 @@
 resource "aiven_project_vpc" "myvpc" {
   project      = aiven_project.myproject.project
   cloud_name   = "google-europe-west1"
-  network_cidr = "192.168.0.1/24"
+  network_cidr = "192.168.1.0/24"
 
   timeouts {
     create = "5m"
