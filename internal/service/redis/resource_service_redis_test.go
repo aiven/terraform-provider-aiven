644d642804012a1f5698895c6081af5023474e51
Aleksander Zaruczewski <zaruczewski@gmail.com>
2022-06-21 15:03:24 +0300

    fix WaitForServiceCreation when Redis persistence is off (#764)

--- internal/service/redis/resource_service_redis_test.go
+++ internal/service/redis/resource_service_redis_test.go
@@ -37,6 +37,21 @@ func TestAccAivenService_redis(t *testing.T) {
 					resource.TestCheckResourceAttr(resourceName, "termination_protection", "false"),
 				),
 			},
+			{
+				Config: testAccRedisServiceResourceWithPersistenceOff(rName),
+				Check: resource.ComposeTestCheckFunc(
+					acc.TestAccCheckAivenServiceCommonAttributes("data.aiven_redis.common"),
+					testAccCheckAivenServiceRedisAttributes("data.aiven_redis.common"),
+					resource.TestCheckResourceAttr(resourceName, "service_name", fmt.Sprintf("test-acc-sr-%s", rName)),
+					resource.TestCheckResourceAttr(resourceName, "project", os.Getenv("AIVEN_PROJECT_NAME")),
+					resource.TestCheckResourceAttr(resourceName, "service_type", "redis"),
+					resource.TestCheckResourceAttr(resourceName, "cloud_name", "google-europe-west1"),
+					resource.TestCheckResourceAttr(resourceName, "maintenance_window_dow", "monday"),
+					resource.TestCheckResourceAttr(resourceName, "maintenance_window_time", "10:00:00"),
+					resource.TestCheckResourceAttr(resourceName, "state", "RUNNING"),
+					resource.TestCheckResourceAttr(resourceName, "termination_protection", "false"),
+				),
+			},
 		},
 	})
 }
@@ -56,6 +71,39 @@ func testAccRedisServiceResource(name string) string {
 		  maintenance_window_time = "10:00:00"
 		
 		  redis_user_config {
+		    redis_maxmemory_policy = "allkeys-random"
+		
+		    public_access {
+		      redis = true
+		    }
+		  }
+		}
+		
+		data "aiven_redis" "common" {
+		  service_name = aiven_redis.bar.service_name
+		  project      = aiven_redis.bar.project
+		
+		  depends_on = [aiven_redis.bar]
+		}`,
+		os.Getenv("AIVEN_PROJECT_NAME"), name)
+}
+
+func testAccRedisServiceResourceWithPersistenceOff(name string) string {
+	return fmt.Sprintf(`
+		data "aiven_project" "foo" {
+		  project = "%s"
+		}
+		
+		resource "aiven_redis" "bar" {
+		  project                 = data.aiven_project.foo.project
+		  cloud_name              = "google-europe-west1"
+		  plan                    = "startup-4"
+		  service_name            = "test-acc-sr-%s"
+		  maintenance_window_dow  = "monday"
+		  maintenance_window_time = "10:00:00"
+		
+		  redis_user_config {
+		    redis_persistence      = "off"
 		    redis_maxmemory_policy = "allkeys-random"
 		
 		    public_access {
