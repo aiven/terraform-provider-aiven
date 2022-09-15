f3477e90468fb03c9c6e61d3ef4fb853901d27d8
Murad Biashimov <murad.biashimov@aiven.io>
2022-09-15 14:06:17 +0200

    add missing docs examples
    	modified:   docs/data-sources/service_component.md

--- examples/resources/aiven_service_integration/resource.tf
+++ examples/resources/aiven_service_integration/resource.tf
@@ -1,9 +1,6 @@
-resource "aiven_service_integration" "myintegration" {
-  project = aiven_project.myproject.project
-  // use destination_endpoint_id or destination_service_name = "aiven_service.YYY.service_name"
-  destination_endpoint_id = aiven_service_integration_endpoint.XX.id
-  integration_type        = "datadog"
-  // use source_service_name or source_endpoint_id = aiven_service_integration_endpoint.XXX.id
-  source_service_name = aiven_kafka.XXX.service_name
+resource "aiven_service_integration" "my_integration_metrics" {
+  project                  = aiven_project.myproject.project
+  integration_type         = "metrics"
+  source_service_name      = aiven_kafka.kfk1.service_name
+  destination_service_name = aiven_m3db.m3db.service_name
 }
-
