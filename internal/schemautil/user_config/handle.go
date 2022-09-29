cf4f493e04e1cc47491e6fdf04453482cf9b4d22
Aleksander Zaruczewski <aleks@aiven.io>
2022-09-29 20:03:00 +0300

    feat: user config SSG, arrays (#904)

--- internal/schemautil/user_config/handle.go
+++ internal/schemautil/user_config/handle.go
@@ -34,9 +34,64 @@ func handleObjectProperty(n string, p map[string]interface{}, t string) *jen.Sta
 	return jen.Values(r)
 }
 
+// handleArrayOfPrimitiveTypeProperty is a function that converts an array of primitive type property to a Terraform
+// schema.
+func handleArrayOfPrimitiveTypeProperty(t string) *jen.Statement {
+	return jen.Op("&").Qual(SchemaPackage, "Schema").Values(jen.Dict{
+		jen.Id("Type"): jen.Qual(SchemaPackage, t),
+	})
+}
+
+// handleArrayOfAggregateTypeProperty is a function that converts an array of aggregate type property to a Terraform
+// schema.
+func handleArrayOfAggregateTypeProperty(ip map[string]interface{}) *jen.Statement {
+	return jen.Op("&").Qual(SchemaPackage, "Resource").Values(jen.Dict{
+		jen.Id("Schema"): jen.Map(jen.String()).Op("*").Qual(SchemaPackage, "Schema").Values(
+			convertPropertiesToSchemaMap(ip),
+		),
+	})
+}
+
 // handleArrayProperty is a function that converts an array type property to a Terraform schema.
 func handleArrayProperty(n string, p map[string]interface{}, t string) *jen.Statement {
-	return jen.Values(convertPropertyToSchema(n, p, t))
+	ia, ok := p["items"].(map[string]interface{})
+	if !ok {
+		return nil
+	}
+
+	// TODO: Handle this case separately.
+	if _, ok := ia["one_of"]; ok {
+		return nil
+	}
+
+	// TODO: Handle this case separately. It accepts both object and string at the same time.
+	if n == "ip_filter" {
+		return nil
+	}
+
+	var e *jen.Statement
+
+	ipa, ok := ia["properties"].(map[string]interface{})
+	if !ok {
+		tn, _ := terraformTypes(slicedString(ia["type"]))
+		if len(tn) > 1 {
+			panic(fmt.Sprintf("multiple Terraform types found for an array of primitive type: %#v", tn))
+		}
+
+		e = handleArrayOfPrimitiveTypeProperty(tn[0])
+	} else {
+		e = handleArrayOfAggregateTypeProperty(ipa)
+	}
+
+	r := convertPropertyToSchema(n, p, t)
+
+	r[jen.Id("Elem")] = e
+
+	if mi, ok := p["max_items"].(int); ok {
+		r[jen.Id("MaxItems")] = jen.Lit(mi)
+	}
+
+	return jen.Values(r)
 }
 
 // handleAggregateTypeProperty is a function that converts an aggregate type property to a Terraform schema.
