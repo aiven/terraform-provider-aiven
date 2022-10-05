35888c1a8d3bc8b257aee1e2a2a2cde0a68faba9
Aleksander Zaruczewski <aleks@aiven.io>
2022-10-05 09:39:01 +0300

    feat: add support for properties like ip_filter (#907)

--- internal/schemautil/user_config/handle.go
+++ internal/schemautil/user_config/handle.go
@@ -4,18 +4,25 @@ import (
 	"fmt"
 
 	"github.com/dave/jennifer/jen"
+	"golang.org/x/exp/maps"
 )
 
 // handlePrimitiveTypeProperty is a function that converts a primitive type property to a Terraform schema.
-func handlePrimitiveTypeProperty(n string, p map[string]interface{}, t string) *jen.Statement {
-	return jen.Values(convertPropertyToSchema(n, p, t))
+func handlePrimitiveTypeProperty(n string, p map[string]interface{}, t string) map[string]*jen.Statement {
+	return map[string]*jen.Statement{n: jen.Values(convertPropertyToSchema(n, p, t))}
 }
 
 // handleObjectProperty is a function that converts an object type property to a Terraform schema.
-func handleObjectProperty(n string, p map[string]interface{}, t string) *jen.Statement {
+func handleObjectProperty(n string, p map[string]interface{}, t string) map[string]*jen.Statement {
 	pa, ok := p["properties"].(map[string]interface{})
 	if !ok {
-		return nil
+		if it, ok := p["items"].(map[string]interface{}); ok {
+			if pa, ok = it["properties"].(map[string]interface{}); !ok {
+				return nil
+			}
+		} else {
+			return nil
+		}
 	}
 
 	r := convertPropertyToSchema(n, p, t)
@@ -31,15 +38,22 @@ func handleObjectProperty(n string, p map[string]interface{}, t string) *jen.Sta
 
 	r[jen.Id("MaxItems")] = jen.Lit(1)
 
-	return jen.Values(r)
+	return map[string]*jen.Statement{n: jen.Values(r)}
 }
 
 // handleArrayOfPrimitiveTypeProperty is a function that converts an array of primitive type property to a Terraform
 // schema.
-func handleArrayOfPrimitiveTypeProperty(t string) *jen.Statement {
-	return jen.Op("&").Qual(SchemaPackage, "Schema").Values(jen.Dict{
+func handleArrayOfPrimitiveTypeProperty(n string, t string) *jen.Statement {
+	r := jen.Dict{
 		jen.Id("Type"): jen.Qual(SchemaPackage, t),
-	})
+	}
+
+	if n == "ip_filter" {
+		// TODO: Add ip_filter_object to this sanity check when DiffSuppressFunc is implemented for it.
+		r[jen.Id("DiffSuppressFunc")] = jen.Qual(SchemaUtilPackage, "IpFilterValueDiffSuppressFunc")
+	}
+
+	return jen.Op("&").Qual(SchemaPackage, "Schema").Values(r)
 }
 
 // handleArrayOfAggregateTypeProperty is a function that converts an array of aggregate type property to a Terraform
@@ -53,7 +67,7 @@ func handleArrayOfAggregateTypeProperty(ip map[string]interface{}) *jen.Statemen
 }
 
 // handleArrayProperty is a function that converts an array type property to a Terraform schema.
-func handleArrayProperty(n string, p map[string]interface{}, t string) *jen.Statement {
+func handleArrayProperty(n string, p map[string]interface{}, t string) map[string]*jen.Statement {
 	ia, ok := p["items"].(map[string]interface{})
 	if !ok {
 		return nil
@@ -64,44 +78,64 @@ func handleArrayProperty(n string, p map[string]interface{}, t string) *jen.Stat
 		return nil
 	}
 
-	// TODO: Handle this case separately. It accepts both object and string at the same time.
-	if n == "ip_filter" {
-		return nil
-	}
-
 	var e *jen.Statement
 
-	ipa, ok := ia["properties"].(map[string]interface{})
-	if !ok {
-		tn, _ := terraformTypes(slicedString(ia["type"]))
+	tn, atn := terraformTypes(slicedString(ia["type"]))
+
+	r := make(map[string]*jen.Statement)
+
+	for k, v := range tn {
+		an := n
 		if len(tn) > 1 {
-			panic(fmt.Sprintf("multiple Terraform types found for an array of primitive type: %#v", tn))
+			an = fmt.Sprintf("%s_%s", n, atn[k])
+
+			// TODO: Remove with the next major version.
+			if an == "ip_filter_string" {
+				an = "ip_filter"
+			}
 		}
 
-		e = handleArrayOfPrimitiveTypeProperty(tn[0])
-	} else {
-		e = handleArrayOfAggregateTypeProperty(ipa)
+		if isTerraformTypePrimitive(v) {
+			e = handleArrayOfPrimitiveTypeProperty(an, v)
+		} else {
+			if ipa, ok := ia["properties"].(map[string]interface{}); ok {
+				e = handleArrayOfAggregateTypeProperty(ipa)
+			} else {
+				panic(fmt.Sprintf("could not find properties in an array of aggregate type: %#v", p))
+			}
+		}
+
+		s := convertPropertyToSchema(n, p, t)
+
+		s[jen.Id("Elem")] = e
+
+		if an == "ip_filter" {
+			// TODO: Add ip_filter_object to this sanity check when DiffSuppressFunc is implemented for it.
+			s[jen.Id("DiffSuppressFunc")] = jen.Qual(SchemaUtilPackage, "IpFilterArrayDiffSuppressFunc")
+		}
+
+		if mi, ok := p["max_items"].(int); ok {
+			s[jen.Id("MaxItems")] = jen.Lit(mi)
+		}
+
+		r[an] = jen.Values(s)
 	}
 
-	r := convertPropertyToSchema(n, p, t)
-
-	r[jen.Id("Elem")] = e
-
-	if mi, ok := p["max_items"].(int); ok {
-		r[jen.Id("MaxItems")] = jen.Lit(mi)
-	}
-
-	return jen.Values(r)
+	return r
 }
 
 // handleAggregateTypeProperty is a function that converts an aggregate type property to a Terraform schema.
-func handleAggregateTypeProperty(n string, p map[string]interface{}, t string, at string) *jen.Statement {
+func handleAggregateTypeProperty(n string, p map[string]interface{}, t string, at string) map[string]*jen.Statement {
+	r := make(map[string]*jen.Statement)
+
 	switch at {
 	case "object":
-		return handleObjectProperty(n, p, t)
+		maps.Copy(r, handleObjectProperty(n, p, t))
 	case "array":
-		return handleArrayProperty(n, p, t)
+		maps.Copy(r, handleArrayProperty(n, p, t))
 	default:
 		panic(fmt.Sprintf("unknown aggregate type: %s", at))
 	}
+
+	return r
 }
