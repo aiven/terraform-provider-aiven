1ba9d0b1f033edcc99a786a895d7006992b4d3e0
Eddie Bachle <ebachle6780@gmail.com>
2022-11-14 10:51:37 -0500

    fix: Update data aiven_project_vpc example
    The `id` field is actually `vpc_id`

--- examples/data-sources/aiven_project_vpc/data-source.tf
+++ examples/data-sources/aiven_project_vpc/data-source.tf
@@ -7,5 +7,5 @@ data "aiven_project_vpc" "myvpc" {
 data "aiven_project_vpc" "myvpc" {
   project    = aiven_project.myproject.project
   cloud_name = "google-europe-west1"
-  id         = aiven_project_vpc.vpc.id
+  vpc_id     = aiven_project_vpc.vpc.id
 }
