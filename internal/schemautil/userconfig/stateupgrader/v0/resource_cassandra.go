40091d32a2db63fb9d19f24f91c5fc17d3ef335b
Aleksander Zaruczewski <aleks@aiven.io>
2023-01-03 13:56:29 +0200

    feat(stateupgrader): cassandra v0 (#1018)

---
+++ internal/schemautil/userconfig/stateupgrader/v0/resource_cassandra.go
@@ -0,0 +1,137 @@
+package v0
+
+import (
+	"context"
+	"time"
+
+	"github.com/aiven/terraform-provider-aiven/internal/schemautil"
+	"github.com/aiven/terraform-provider-aiven/internal/schemautil/userconfig/stateupgrader/typeupgrader"
+	"github.com/aiven/terraform-provider-aiven/internal/schemautil/userconfig/stateupgrader/v0/dist"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/customdiff"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func cassandraSchema() map[string]*schema.Schema {
+	s := schemautil.ServiceCommonSchema()
+	s[schemautil.ServiceTypeCassandra] = &schema.Schema{
+		Type:        schema.TypeList,
+		Computed:    true,
+		Description: "Cassandra server provided values",
+		Elem: &schema.Resource{
+			Schema: map[string]*schema.Schema{},
+		},
+	}
+	s[schemautil.ServiceTypeCassandra+"_user_config"] = dist.ServiceTypeCassandra()
+
+	return s
+}
+
+func ResourceCassandraResourceV0() *schema.Resource {
+	return &schema.Resource{
+		Description:   "The Cassandra resource allows the creation and management of Aiven Cassandra services.",
+		CreateContext: schemautil.ResourceServiceCreateWrapper(schemautil.ServiceTypeCassandra),
+		ReadContext:   schemautil.ResourceServiceRead,
+		UpdateContext: schemautil.ResourceServiceUpdate,
+		DeleteContext: schemautil.ResourceServiceDelete,
+		CustomizeDiff: customdiff.Sequence(
+			schemautil.SetServiceTypeIfEmpty(schemautil.ServiceTypeCassandra),
+			schemautil.CustomizeDiffDisallowMultipleManyToOneKeys,
+			customdiff.IfValueChange("tag",
+				schemautil.TagsShouldNotBeEmpty,
+				schemautil.CustomizeDiffCheckUniqueTag,
+			),
+			customdiff.IfValueChange("disk_space",
+				schemautil.DiskSpaceShouldNotBeEmpty,
+				schemautil.CustomizeDiffCheckDiskSpace,
+			),
+			customdiff.IfValueChange("additional_disk_space",
+				schemautil.DiskSpaceShouldNotBeEmpty,
+				schemautil.CustomizeDiffCheckDiskSpace,
+			),
+			customdiff.IfValueChange("service_integrations",
+				schemautil.ServiceIntegrationShouldNotBeEmpty,
+				schemautil.CustomizeDiffServiceIntegrationAfterCreation,
+			),
+			customdiff.Sequence(
+				schemautil.CustomizeDiffCheckPlanAndStaticIpsCannotBeModifiedTogether,
+				schemautil.CustomizeDiffCheckStaticIpDisassociation,
+			),
+		),
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(20 * time.Minute),
+			Update: schema.DefaultTimeout(20 * time.Minute),
+			Delete: schema.DefaultTimeout(20 * time.Minute),
+		},
+
+		Schema: cassandraSchema(),
+	}
+}
+
+func ResourceCassandraStateUpgradeV0(
+	_ context.Context,
+	rawState map[string]interface{},
+	_ interface{},
+) (map[string]interface{}, error) {
+	userConfigSlice, ok := rawState["cassandra_user_config"].([]interface{})
+	if !ok {
+		return rawState, nil
+	}
+
+	userConfig, ok := userConfigSlice[0].(map[string]interface{})
+	if !ok {
+		return rawState, nil
+	}
+
+	err := typeupgrader.Map(userConfig, map[string]string{
+		"migrate_sstableloader": "bool",
+		"static_ips":            "bool",
+	})
+	if err != nil {
+		return rawState, err
+	}
+
+	cassandraSlice, ok := userConfig["cassandra"].([]interface{})
+	if ok && len(cassandraSlice) > 0 {
+		cassandra, ok := cassandraSlice[0].(map[string]interface{})
+		if ok {
+			err := typeupgrader.Map(cassandra, map[string]string{
+				"batch_size_fail_threshold_in_kb": "int",
+				"batch_size_warn_threshold_in_kb": "int",
+			})
+			if err != nil {
+				return rawState, err
+			}
+		}
+	}
+
+	privateAccessSlice, ok := userConfig["private_access"].([]interface{})
+	if ok && len(privateAccessSlice) > 0 {
+		privateAccess, ok := privateAccessSlice[0].(map[string]interface{})
+		if ok {
+			err = typeupgrader.Map(privateAccess, map[string]string{
+				"prometheus": "bool",
+			})
+			if err != nil {
+				return rawState, err
+			}
+		}
+	}
+
+	publicAccessSlice, ok := userConfig["public_access"].([]interface{})
+	if ok && len(publicAccessSlice) > 0 {
+		publicAccess, ok := publicAccessSlice[0].(map[string]interface{})
+		if ok {
+			err := typeupgrader.Map(publicAccess, map[string]string{
+				"prometheus": "bool",
+			})
+			if err != nil {
+				return rawState, err
+			}
+		}
+	}
+
+	return rawState, nil
+}
