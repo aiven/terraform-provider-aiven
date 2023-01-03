d1691493225f72d05bfdf2475857b88a40c0481e
Aleksander Zaruczewski <aleks@aiven.io>
2023-01-03 13:56:29 +0200

    feat(stateupgrader): cassandra v0 (#1018)

--- internal/schemautil/userconfig/stateupgrader/upgraders.go
+++ internal/schemautil/userconfig/stateupgrader/upgraders.go
@@ -5,6 +5,16 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
+func Cassandra() []schema.StateUpgrader {
+	return []schema.StateUpgrader{
+		{
+			Type:    v0.ResourceCassandraResourceV0().CoreConfigSchema().ImpliedType(),
+			Upgrade: v0.ResourceCassandraStateUpgradeV0,
+			Version: 0,
+		},
+	}
+}
+
 func Grafana() []schema.StateUpgrader {
 	return []schema.StateUpgrader{
 		{
