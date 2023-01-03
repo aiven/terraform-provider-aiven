823ad48e6f01b448c812b88b9ab6a6ef4bbb0c22
Aleksander Zaruczewski <aleks@aiven.io>
2023-01-03 15:56:56 +0200

    feat(stateupgrader): redis v0

--- internal/service/redis/resource_redis.go
+++ internal/service/redis/resource_redis.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/aiven/terraform-provider-aiven/internal/schemautil"
 	"github.com/aiven/terraform-provider-aiven/internal/schemautil/userconfig/dist"
+	"github.com/aiven/terraform-provider-aiven/internal/schemautil/userconfig/stateupgrader"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/customdiff"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
@@ -64,6 +65,8 @@ func ResourceRedis() *schema.Resource {
 			Delete: schema.DefaultTimeout(20 * time.Minute),
 		},
 
-		Schema: redisSchema(),
+		Schema:         redisSchema(),
+		SchemaVersion:  1,
+		StateUpgraders: stateupgrader.Redis(),
 	}
 }
