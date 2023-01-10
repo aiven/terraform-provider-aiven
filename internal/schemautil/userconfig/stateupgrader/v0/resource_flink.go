5acb76b48c3ede2b48316383b05026e15d1a6a34
Aleksander Zaruczewski <aleks@aiven.io>
2023-01-10 18:04:06 +0200

    feat(stateupgrader): flink v0 (#1019)

--- internal/schemautil/userconfig/stateupgrader/v0/resource_flink.go
+++ internal/schemautil/userconfig/stateupgrader/v0/resource_flink.go
@@ -1,12 +1,12 @@
-package flink
+package v0
 
 import (
+	"context"
 	"time"
 
 	"github.com/aiven/terraform-provider-aiven/internal/schemautil"
-	"github.com/aiven/terraform-provider-aiven/internal/schemautil/userconfig/dist"
-	"github.com/aiven/terraform-provider-aiven/internal/schemautil/userconfig/stateupgrader"
-
+	"github.com/aiven/terraform-provider-aiven/internal/schemautil/userconfig/stateupgrader/typeupgrader"
+	"github.com/aiven/terraform-provider-aiven/internal/schemautil/userconfig/stateupgrader/v0/dist"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/customdiff"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
@@ -38,7 +38,7 @@ func aivenFlinkSchema() map[string]*schema.Schema {
 	return aivenFlinkSchema
 }
 
-func ResourceFlink() *schema.Resource {
+func ResourceFlinkResourceV0() *schema.Resource {
 	return &schema.Resource{
 		Description:   "The Flink resource allows the creation and management of Aiven Flink services.",
 		CreateContext: schemautil.ResourceServiceCreateWrapper(schemautil.ServiceTypeFlink),
@@ -66,7 +66,7 @@ func ResourceFlink() *schema.Resource {
 			),
 			customdiff.Sequence(
 				schemautil.CustomizeDiffCheckPlanAndStaticIpsCannotBeModifiedTogether,
-				schemautil.CustomizeDiffCheckStaticIPDisassociation,
+				schemautil.CustomizeDiffCheckStaticIpDisassociation,
 			),
 		),
 		Importer: &schema.ResourceImporter{
@@ -78,8 +78,51 @@ func ResourceFlink() *schema.Resource {
 			Delete: schema.DefaultTimeout(20 * time.Minute),
 		},
 
-		Schema:         aivenFlinkSchema(),
-		SchemaVersion:  1,
-		StateUpgraders: stateupgrader.Flink(),
+		Schema: aivenFlinkSchema(),
 	}
+}
+
+func ResourceFlinkStateUpgradeV0(
+	_ context.Context,
+	rawState map[string]interface{},
+	_ interface{},
+) (map[string]interface{}, error) {
+	userConfigSlice, ok := rawState["flink_user_config"].([]interface{})
+	if !ok {
+		return rawState, nil
+	}
+
+	userConfig, ok := userConfigSlice[0].(map[string]interface{})
+	if !ok {
+		return rawState, nil
+	}
+
+	err := typeupgrader.Map(userConfig, map[string]string{
+		"execution_checkpointing_interval_ms":        "int",
+		"execution_checkpointing_timeout_ms":         "int",
+		"number_of_task_slots":                       "int",
+		"parallelism_default":                        "int",
+		"restart_strategy_delay_sec":                 "int",
+		"restart_strategy_failure_rate_interval_min": "int",
+		"restart_strategy_max_failures":              "int",
+	})
+	if err != nil {
+		return rawState, err
+	}
+
+	privateLinkAccessSlice, ok := userConfig["privatelink_access"].([]interface{})
+	if ok && len(privateLinkAccessSlice) > 0 {
+		privateLinkAccess, ok := privateLinkAccessSlice[0].(map[string]interface{})
+		if ok {
+			err := typeupgrader.Map(privateLinkAccess, map[string]string{
+				"flink":      "bool",
+				"prometheus": "bool",
+			})
+			if err != nil {
+				return rawState, err
+			}
+		}
+	}
+
+	return rawState, nil
 }
