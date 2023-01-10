b5c4d455bb9a0ccd135ef83fa3f10e158a9b9c8d
Aleksander Zaruczewski <aleks@aiven.io>
2023-01-10 18:06:34 +0200

    feat(stateupgrader): m3db v0 (#1023)

--- internal/schemautil/userconfig/stateupgrader/v0/resource_m3aggregator.go
+++ internal/schemautil/userconfig/stateupgrader/v0/resource_m3aggregator.go
@@ -1,9 +1,12 @@
-package m3db
+package v0
 
 import (
+	"context"
+	"time"
+
 	"github.com/aiven/terraform-provider-aiven/internal/schemautil"
-	"github.com/aiven/terraform-provider-aiven/internal/schemautil/userconfig/dist"
-	"github.com/aiven/terraform-provider-aiven/internal/schemautil/userconfig/stateupgrader"
+	"github.com/aiven/terraform-provider-aiven/internal/schemautil/userconfig/stateupgrader/typeupgrader"
+	"github.com/aiven/terraform-provider-aiven/internal/schemautil/userconfig/stateupgrader/v0/dist"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/customdiff"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
@@ -22,7 +25,7 @@ func aivenM3AggregatorSchema() map[string]*schema.Schema {
 
 	return schemaM3
 }
-func ResourceM3Aggregator() *schema.Resource {
+func ResourceM3AggregatorResourceV0() *schema.Resource {
 	return &schema.Resource{
 		Description:   "The M3 Aggregator resource allows the creation and management of Aiven M3 Aggregator services.",
 		CreateContext: schemautil.ResourceServiceCreateWrapper(schemautil.ServiceTypeM3Aggregator),
@@ -46,16 +49,43 @@ func ResourceM3Aggregator() *schema.Resource {
 			),
 			customdiff.Sequence(
 				schemautil.CustomizeDiffCheckPlanAndStaticIpsCannotBeModifiedTogether,
-				schemautil.CustomizeDiffCheckStaticIPDisassociation,
+				schemautil.CustomizeDiffCheckStaticIpDisassociation,
 			),
 		),
 		Importer: &schema.ResourceImporter{
 			StateContext: schema.ImportStatePassthroughContext,
 		},
-		Timeouts: schemautil.DefaultResourceTimeouts(),
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(20 * time.Minute),
+			Update: schema.DefaultTimeout(20 * time.Minute),
+			Delete: schema.DefaultTimeout(20 * time.Minute),
+		},
 
-		Schema:         aivenM3AggregatorSchema(),
-		SchemaVersion:  1,
-		StateUpgraders: stateupgrader.M3Aggregator(),
+		Schema: aivenM3AggregatorSchema(),
 	}
+}
+
+func ResourceM3AggregatorStateUpgradeV0(
+	_ context.Context,
+	rawState map[string]interface{},
+	_ interface{},
+) (map[string]interface{}, error) {
+	userConfigSlice, ok := rawState["m3aggregator_user_config"].([]interface{})
+	if !ok {
+		return rawState, nil
+	}
+
+	userConfig, ok := userConfigSlice[0].(map[string]interface{})
+	if !ok {
+		return rawState, nil
+	}
+
+	err := typeupgrader.Map(userConfig, map[string]string{
+		"static_ips": "bool",
+	})
+	if err != nil {
+		return rawState, err
+	}
+
+	return rawState, nil
 }
