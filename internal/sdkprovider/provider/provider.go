ab9438fc9ad22edb2d3ab5bbe53e66736923a4b4
Ivan Savciuc <ivan.savciuc@aiven.io>
2023-09-25 20:37:42 +0300

    feat(org): add user group member support

--- internal/sdkprovider/provider/provider.go
+++ internal/sdkprovider/provider/provider.go
@@ -181,9 +181,10 @@ func Provider(version string) *schema.Provider {
 			"aiven_account_authentication": account.ResourceAccountAuthentication(),
 
 			// organization
-			"aiven_organizational_unit":     organization.ResourceOrganizationalUnit(),
-			"aiven_organization_user":       organization.ResourceOrganizationUser(),
-			"aiven_organization_user_group": organization.ResourceOrganizationUserGroup(),
+			"aiven_organizational_unit":            organization.ResourceOrganizationalUnit(),
+			"aiven_organization_user":              organization.ResourceOrganizationUser(),
+			"aiven_organization_user_group":        organization.ResourceOrganizationUserGroup(),
+			"aiven_organization_user_group_member": organization.ResourceOrganizationUserGroupMember(),
 
 			// project
 			"aiven_project":       project.ResourceProject(),
