39388c430ee9ae1c4bd0778f0c38392198352041
Ivan Savciuc <ivan.savciuc@aiven.io>
2024-01-05 12:58:13 +0200

    feat: add service integration endpoint support for new user config generator

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ import (
 //go:generate go test -tags userconfig ./internal/schemautil/userconfig
 //go:generate go run ./ucgenerator/... --services cassandra,clickhouse,flink,grafana,influxdb,kafka,kafka_connect,kafka_mirrormaker,m3aggregator,m3db,mysql,opensearch,pg,redis
 //go:generate go run ./ucgenerator/... --integrations clickhouse_kafka,clickhouse_postgresql,datadog,external_aws_cloudwatch_logs,external_aws_cloudwatch_metrics,external_elasticsearch_logs,external_opensearch_logs,kafka_connect,kafka_logs,kafka_mirrormaker,logs,metrics,prometheus
+//go:generate go run ./ucgenerator/... --integration-endpoints datadog,external_aws_cloudwatch_logs,external_aws_cloudwatch_metrics,external_elasticsearch_logs,external_google_cloud_bigquery,external_google_cloud_logging,external_kafka,external_opensearch_logs,external_postgresql,external_schema_registry,jolokia,prometheus,rsyslog
 const registryPrefix = "registry.terraform.io/"
 
 // version is the version of the provider.
