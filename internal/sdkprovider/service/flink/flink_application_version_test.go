08c8dc7021c3159a4a6b605cafcd11c1a49ba2f8
Aleksander Zaruczewski <aleks@aiven.io>
2024-02-23 13:45:05 +0200

    test(flink_application_version): fix incorrect sql

--- internal/sdkprovider/service/flink/flink_application_version_test.go
+++ internal/sdkprovider/service/flink/flink_application_version_test.go
@@ -133,13 +133,37 @@ resource "aiven_flink_application_version" "foo" {
   project        = data.aiven_project.foo.project
   service_name   = aiven_flink.foo.service_name
   application_id = aiven_flink_application.foo.application_id
-  statement      = "INSERT INTO kafka_known_pizza SELECT * FROM kafka_pizza WHERE shop LIKE 'Luigis Pizza'"
+  statement      = <<EOT
+    INSERT INTO kafka_known_pizza SELECT * FROM kafka_pizza WHERE shop LIKE '%%Luigis Pizza%%'
+  EOT
   sink {
-    create_table   = "CREATE TABLE kafka_known_pizza (shop STRING,name STRING) WITH ('connector' = 'kafka','properties.bootstrap.servers' = '','scan.startup.mode' = 'earliest-offset','topic' = 'test_out','value.format' = 'json')"
+    create_table   = <<EOT
+      CREATE TABLE kafka_known_pizza (
+        shop STRING,
+        name STRING
+      ) WITH (
+        'connector' = 'kafka',
+        'properties.bootstrap.servers' = '',
+        'scan.startup.mode' = 'earliest-offset',
+        'topic' = 'sink_topic',
+        'value.format' = 'json'
+      )
+    EOT
     integration_id = aiven_service_integration.flink_to_kafka.integration_id
   }
   source {
-    create_table   = "CREATE TABLE kafka_pizza (shop STRING, name STRING) WITH ('connector' = 'kafka','properties.bootstrap.servers' = '','scan.startup.mode' = 'earliest-offset','topic' = 'test','value.format' = 'json')"
+    create_table   = <<EOT
+      CREATE TABLE kafka_pizza (
+        shop STRING,
+        name STRING
+      ) WITH (
+        'connector' = 'kafka',
+        'properties.bootstrap.servers' = '',
+        'scan.startup.mode' = 'earliest-offset',
+        'topic' = 'source_topic',
+        'value.format' = 'json'
+      )
+    EOT
     integration_id = aiven_service_integration.flink_to_kafka.integration_id
   }
 }
