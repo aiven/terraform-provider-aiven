093eb3ad2c9e0b17cc8cd4bf15a9244fcae96676
Stacey Salamon <stacey.salamon@aiven.io>
2024-03-22 14:26:14 +0100

    docs: remove duplicate example usage

--- examples/resources/aiven_organization_group_project/resource.tf
+++ examples/resources/aiven_organization_group_project/resource.tf
@@ -1,3 +1,7 @@
+resource "aiven_project" "example_project" {
+  project    = "Example project"
+  parent_id = aiven_organization.main.id
+}
 
 resource "aiven_organization_user_group" "example" {
   description = "Example group of users."
@@ -5,8 +9,14 @@ resource "aiven_organization_user_group" "example" {
   name = "Example group"
 }
 
+resource "aiven_organization_user_group_member" "project_admin" {
+  group_id = aiven_organization_user_group.example.group_id
+  organization_id = aiven_organization.main.id
+  user_id = "u123a456b7890c" 
+}
+
 resource "aiven_organization_group_project" "example" {
   group_id = aiven_organization_user_group.example.group_id
-  project = aiven_project.example.project
+  project = aiven_project.example_project.project
   role = "admin"
 }
