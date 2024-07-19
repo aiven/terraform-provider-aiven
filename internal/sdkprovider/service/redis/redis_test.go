1c937d143af90e41887a080bf8cb73b08ce5c4ad
Bogdan Petre <bogdan.petre@aiven.io>
2024-07-19 15:13:17 +0200

    refactor: remove redundant redis test

--- internal/sdkprovider/service/redis/redis_test.go
+++ internal/sdkprovider/service/redis/redis_test.go
@@ -56,6 +56,21 @@ func TestAccAiven_redis(t *testing.T) {
 				),
 			},
 			{
+				Config: testAccRedisServiceResourceWithPersistenceOff(rName),
+				Check: resource.ComposeTestCheckFunc(
+					acc.TestAccCheckAivenServiceCommonAttributes("data.aiven_redis.common"),
+					testAccCheckAivenServiceRedisAttributes("data.aiven_redis.common"),
+					resource.TestCheckResourceAttr(resourceName, "service_name", fmt.Sprintf("test-acc-sr-%s", rName)),
+					resource.TestCheckResourceAttr(resourceName, "project", os.Getenv("AIVEN_PROJECT_NAME")),
+					resource.TestCheckResourceAttr(resourceName, "service_type", "redis"),
+					resource.TestCheckResourceAttr(resourceName, "cloud_name", "google-europe-west1"),
+					resource.TestCheckResourceAttr(resourceName, "maintenance_window_dow", "monday"),
+					resource.TestCheckResourceAttr(resourceName, "maintenance_window_time", "10:00:00"),
+					resource.TestCheckResourceAttr(resourceName, "state", "RUNNING"),
+					resource.TestCheckResourceAttr(resourceName, "termination_protection", "false"),
+				),
+			},
+			{
 				Config:             testAccRedisDoubleTagResource(rName),
 				PlanOnly:           true,
 				ExpectNonEmptyPlan: true,
@@ -141,6 +156,38 @@ data "aiven_redis" "common" {
 }`, os.Getenv("AIVEN_PROJECT_NAME"), name)
 }
 
+func testAccRedisServiceResourceWithPersistenceOff(name string) string {
+	return fmt.Sprintf(`
+data "aiven_project" "foo" {
+  project = "%s"
+}
+
+resource "aiven_redis" "bar" {
+  project                 = data.aiven_project.foo.project
+  cloud_name              = "google-europe-west1"
+  plan                    = "startup-4"
+  service_name            = "test-acc-sr-%s"
+  maintenance_window_dow  = "monday"
+  maintenance_window_time = "10:00:00"
+
+  redis_user_config {
+    redis_persistence      = "off"
+    redis_maxmemory_policy = "allkeys-random"
+
+    public_access {
+      redis = true
+    }
+  }
+}
+
+data "aiven_redis" "common" {
+  service_name = aiven_redis.bar.service_name
+  project      = aiven_redis.bar.project
+
+  depends_on = [aiven_redis.bar]
+}`, os.Getenv("AIVEN_PROJECT_NAME"), name)
+}
+
 func testAccRedisDoubleTagResource(name string) string {
 	return fmt.Sprintf(`
 data "aiven_project" "foo" {
@@ -176,113 +223,6 @@ resource "aiven_redis" "bar" {
 data "aiven_redis" "common" {
   service_name = aiven_redis.bar.service_name
   project      = data.aiven_project.foo.project
-
-  depends_on = [aiven_redis.bar]
-}`, os.Getenv("AIVEN_PROJECT_NAME"), name)
-}
-
-// Redis service tests
-func TestAccAivenService_redis(t *testing.T) {
-	resourceName := "aiven_redis.bar"
-	rName := acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum)
-
-	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:                 func() { acc.TestAccPreCheck(t) },
-		ProtoV6ProviderFactories: acc.TestProtoV6ProviderFactories,
-		CheckDestroy:             acc.TestAccCheckAivenServiceResourceDestroy,
-		Steps: []resource.TestStep{
-			{
-				Config: testAccRedisServiceResource(rName),
-				Check: resource.ComposeTestCheckFunc(
-					acc.TestAccCheckAivenServiceCommonAttributes("data.aiven_redis.common"),
-					testAccCheckAivenServiceRedisAttributes("data.aiven_redis.common"),
-					resource.TestCheckResourceAttr(resourceName, "service_name", fmt.Sprintf("test-acc-sr-%s", rName)),
-					resource.TestCheckResourceAttr(resourceName, "project", os.Getenv("AIVEN_PROJECT_NAME")),
-					resource.TestCheckResourceAttr(resourceName, "service_type", "redis"),
-					resource.TestCheckResourceAttr(resourceName, "cloud_name", "google-europe-west1"),
-					resource.TestCheckResourceAttr(resourceName, "maintenance_window_dow", "monday"),
-					resource.TestCheckResourceAttr(resourceName, "maintenance_window_time", "10:00:00"),
-					resource.TestCheckResourceAttr(resourceName, "state", "RUNNING"),
-					resource.TestCheckResourceAttr(resourceName, "termination_protection", "false"),
-				),
-			},
-			{
-				Config: testAccRedisServiceResourceWithPersistenceOff(rName),
-				Check: resource.ComposeTestCheckFunc(
-					acc.TestAccCheckAivenServiceCommonAttributes("data.aiven_redis.common"),
-					testAccCheckAivenServiceRedisAttributes("data.aiven_redis.common"),
-					resource.TestCheckResourceAttr(resourceName, "service_name", fmt.Sprintf("test-acc-sr-%s", rName)),
-					resource.TestCheckResourceAttr(resourceName, "project", os.Getenv("AIVEN_PROJECT_NAME")),
-					resource.TestCheckResourceAttr(resourceName, "service_type", "redis"),
-					resource.TestCheckResourceAttr(resourceName, "cloud_name", "google-europe-west1"),
-					resource.TestCheckResourceAttr(resourceName, "maintenance_window_dow", "monday"),
-					resource.TestCheckResourceAttr(resourceName, "maintenance_window_time", "10:00:00"),
-					resource.TestCheckResourceAttr(resourceName, "state", "RUNNING"),
-					resource.TestCheckResourceAttr(resourceName, "termination_protection", "false"),
-				),
-			},
-		},
-	})
-}
-
-func testAccRedisServiceResource(name string) string {
-	return fmt.Sprintf(`
-data "aiven_project" "foo" {
-  project = "%s"
-}
-
-resource "aiven_redis" "bar" {
-  project                 = data.aiven_project.foo.project
-  cloud_name              = "google-europe-west1"
-  plan                    = "startup-4"
-  service_name            = "test-acc-sr-%s"
-  maintenance_window_dow  = "monday"
-  maintenance_window_time = "10:00:00"
-
-  redis_user_config {
-    redis_maxmemory_policy = "allkeys-random"
-
-    public_access {
-      redis = true
-    }
-  }
-}
-
-data "aiven_redis" "common" {
-  service_name = aiven_redis.bar.service_name
-  project      = aiven_redis.bar.project
-
-  depends_on = [aiven_redis.bar]
-}`, os.Getenv("AIVEN_PROJECT_NAME"), name)
-}
-
-func testAccRedisServiceResourceWithPersistenceOff(name string) string {
-	return fmt.Sprintf(`
-data "aiven_project" "foo" {
-  project = "%s"
-}
-
-resource "aiven_redis" "bar" {
-  project                 = data.aiven_project.foo.project
-  cloud_name              = "google-europe-west1"
-  plan                    = "startup-4"
-  service_name            = "test-acc-sr-%s"
-  maintenance_window_dow  = "monday"
-  maintenance_window_time = "10:00:00"
-
-  redis_user_config {
-    redis_persistence      = "off"
-    redis_maxmemory_policy = "allkeys-random"
-
-    public_access {
-      redis = true
-    }
-  }
-}
-
-data "aiven_redis" "common" {
-  service_name = aiven_redis.bar.service_name
-  project      = aiven_redis.bar.project
 
   depends_on = [aiven_redis.bar]
 }`, os.Getenv("AIVEN_PROJECT_NAME"), name)
