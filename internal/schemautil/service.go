4c726238b5ef73341b13c362b9192fc7358ea41e
Timo Riski <timo.riski@aiven.io>
2024-09-18 13:28:39 +0300

    fix: mark project_vpc_id field as computed

--- internal/schemautil/service.go
+++ internal/schemautil/service.go
@@ -106,6 +106,7 @@ func ServiceCommonSchema() map[string]*schema.Schema {
 		"project_vpc_id": {
 			Type:        schema.TypeString,
 			Optional:    true,
+			Computed:    true,
 			Description: "Specifies the VPC the service should run in. If the value is not set the service is not run inside a VPC. When set, the value should be given as a reference to set up dependencies correctly and the VPC must be in the same cloud and region as the service itself. Project can be freely moved to and from VPC after creation but doing so triggers migration to new servers so the operation can take significant amount of time to complete if the service has a lot of data.",
 		},
 		"maintenance_window_dow": {
