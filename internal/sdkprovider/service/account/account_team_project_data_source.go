a0ad7e5cb1ebbed01e57a1d73d6d1119ba2502aa
Myroslav Vivcharyk <myroslav.vivcharyk@aiven.io>
2025-01-02 12:56:51 +0100

    refactor(account_team_project): replaced client-v2 with avngen

--- internal/sdkprovider/service/account/account_team_project_data_source.go
+++ internal/sdkprovider/service/account/account_team_project_data_source.go
@@ -3,27 +3,28 @@ package account
 import (
 	"context"
 
-	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	avngen "github.com/aiven/go-client-codegen"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 
+	"github.com/aiven/terraform-provider-aiven/internal/common"
 	"github.com/aiven/terraform-provider-aiven/internal/schemautil"
 )
 
 func DatasourceAccountTeamProject() *schema.Resource {
 	return &schema.Resource{
-		ReadContext: datasourceAccountTeamProjectRead,
+		ReadContext: common.WithGenClient(datasourceAccountTeamProjectRead),
 		Description: "The Account Team Project data source provides information about the existing Account Team Project.",
 		Schema: schemautil.ResourceSchemaAsDatasourceSchema(aivenAccountTeamProjectSchema,
 			"account_id", "team_id", "project_name"),
 	}
 }
 
-func datasourceAccountTeamProjectRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+func datasourceAccountTeamProjectRead(ctx context.Context, d *schema.ResourceData, client avngen.Client) error {
 	accountID := d.Get("account_id").(string)
 	teamID := d.Get("team_id").(string)
 	projectName := d.Get("project_name").(string)
 
 	d.SetId(schemautil.BuildResourceID(accountID, teamID, projectName))
 
-	return resourceAccountTeamProjectRead(ctx, d, m)
+	return resourceAccountTeamProjectRead(ctx, d, client)
 }
