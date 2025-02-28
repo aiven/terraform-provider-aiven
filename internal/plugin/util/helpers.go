99c549d4132a188ed781ac2a03b7b4c731430eb4
Timo Riski <timo.riski@aiven.io>
2025-02-28 17:06:43 +0200

    chore: remove unused `First` and `ToBigFloat` helpers

--- internal/plugin/util/helpers.go
+++ internal/plugin/util/helpers.go
@@ -1,11 +1,5 @@
 package util
 
-import (
-	"math/big"
-
-	"golang.org/x/exp/constraints"
-)
-
 // This file contains helper functions that are more generic and can be used in multiple places.
 // These functions are not specific to the Aiven plugin. If you are looking for Aiven plugin specific helpers,
 // please see the pluginhelpers.go file instead.
@@ -23,14 +17,4 @@ func NilIfZero[T comparable](v T) *T {
 	}
 
 	return &v
-}
-
-// First is a helper function that returns the first argument passed in out of two.
-func First[T any, U any](a T, _ U) T {
-	return a
-}
-
-// ToBigFloat is a helper function that converts any integer or float type to a big.Float.
-func ToBigFloat[T constraints.Integer | constraints.Float](v T) *big.Float {
-	return big.NewFloat(float64(v))
 }
