04542f6334d587321907657854be71f4e70cbd66
Stacey Salamon <stacey.salamon@aiven.io>
2025-03-04 17:33:04 +0100

    docs: add warning to project user and group project resources

--- internal/sdkprovider/service/project/project_user.go
+++ internal/sdkprovider/service/project/project_user.go
@@ -51,7 +51,7 @@ func ResourceProjectUser() *schema.Resource {
 		Schema: aivenProjectUserSchema,
 		DeprecationMessage: `Use aiven_organization_permission instead and
 [migrate existing aiven_project_user resources](https://registry.terraform.io/providers/aiven/aiven/latest/docs/guides/update-deprecated-resources) 
-to the new resource.`,
+to the new resource. **Do not use the aiven_project_user and aiven_organization_permission resources together**.`,
 	}
 }
 
