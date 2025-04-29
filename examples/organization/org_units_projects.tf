f7b8673f51b6ccc33e8ee86eb7e0e66c1e7e107a
Stacey Salamon <stacey.salamon@aiven.io>
2025-04-29 18:06:06 +0200

    docs: update orgs and units example for inclusion in Aiven Docs

--- examples/organization/org_units_projects.tf
+++ examples/organization/org_units_projects.tf
@@ -1,8 +1,3 @@
-variable "aiven_api_token" {}
-variable "prod_project_name" {}
-variable "qa_project_name" {}
-variable "dev_project_name" {}
-
 terraform {
   required_version = ">=0.13"
   required_providers {
@@ -14,57 +9,55 @@ terraform {
 }
 
 provider "aiven" {
-  api_token = var.aiven_api_token
+  api_token = var.aiven_token
 }
 
-
-# Create organization
-resource "aiven_organization" "org" {
-  name = "Example Organization"
+# Your organization
+data "aiven_organization" "org" {
+  name = var.organization_name
 }
 
-
-# Create units within organization
+# Create units within your organization
 resource "aiven_organizational_unit" "unit-eng" {
-  name = "Engineering"
-  parent_id = aiven_organization.org.id
+  name      = "Engineering"
+  parent_id = data.aiven_organization.org.id
 }
 
 resource "aiven_organizational_unit" "unit-fin" {
-  name = "Finance"
-  parent_id = aiven_organization.org.id
+  name      = "Finance"
+  parent_id = data.aiven_organization.org.id
 }
 
 # Create projects in units
 
 # Engineering projects
 resource "aiven_project" "prod-eng" {
-  project    = "${var.prod_project_name}-eng"
+  project   = "${var.prod_project_name}-eng"
   parent_id = aiven_organizational_unit.unit-eng.id
 }
 
 resource "aiven_project" "qa-eng" {
-  project    = "${var.qa_project_name}-eng"
+  project   = "${var.qa_project_name}-eng"
   parent_id = aiven_organizational_unit.unit-eng.id
 }
 
 resource "aiven_project" "dev-eng" {
-  project    = "${var.dev_project_name}-eng"
+  project   = "${var.dev_project_name}-eng"
   parent_id = aiven_organizational_unit.unit-eng.id
 }
 
 # Finance projects
 resource "aiven_project" "prod-fin" {
-  project    = "${var.prod_project_name}-fin"
+  project   = "${var.prod_project_name}-fin"
   parent_id = aiven_organizational_unit.unit-fin.id
 }
 
 resource "aiven_project" "qa-fin" {
-  project    = "${var.qa_project_name}-fin"
+  project   = "${var.qa_project_name}-fin"
   parent_id = aiven_organizational_unit.unit-fin.id
 }
 
 resource "aiven_project" "dev-fin" {
-  project    = "${var.dev_project_name}-fin"
+  project   = "${var.dev_project_name}-fin"
   parent_id = aiven_organizational_unit.unit-fin.id
 }
