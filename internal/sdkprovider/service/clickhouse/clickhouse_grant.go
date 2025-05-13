c585279abd69645acf7ecb6203c0671d77a70a22
Stacey Salamon <stacey.salamon@aiven.io>
2025-05-13 15:31:44 +0200

    docs(clickhouse): add instructions for granting global privileges

--- internal/sdkprovider/service/clickhouse/clickhouse_grant.go
+++ internal/sdkprovider/service/clickhouse/clickhouse_grant.go
@@ -100,6 +100,7 @@ func ResourceClickhouseGrant() *schema.Resource {
 
 **Note:**
 * Users cannot have the same name as roles.
+* Global privileges cannot be granted on the database level. To grant global privileges, use ` + "`database=\"*\"`" + `.
 * To grant a privilege on all tables of a database, omit the table and only keep the database. Don't use ` + "`table=\"*\"`" + `.
 * Changes first revoke all grants and then reissue the remaining grants for convergence.
 `,
