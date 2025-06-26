fece6cc34c795846b0f04a184c88b749cccba5c3
Stacey Salamon <stacey.salamon@aiven.io>
2025-06-26 16:42:26 +0200

    docs(kafka): update description for diskless topics

--- internal/sdkprovider/service/kafkatopic/kafka_topic.go
+++ internal/sdkprovider/service/kafkatopic/kafka_topic.go
@@ -171,7 +171,7 @@ func aivenKafkaTopicConfigSchema() map[string]*schema.Schema {
 		},
 		"inkless_enable": {
 			Type:        schema.TypeBool,
-			Description: "Indicates whether inkless should be enabled. This is only available for BYOC services with Inkless feature enabled.",
+			Description: "Creates a [diskless topic](https://aiven.io/docs/products/diskless). You can only do this when you create the topic and you cannot change it later. Diskless topics are only available for bring your own cloud (BYOC) services that have the feature enabled.",
 			Optional:    true,
 		},
 	}
