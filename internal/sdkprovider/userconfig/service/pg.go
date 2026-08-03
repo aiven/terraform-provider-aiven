5dbcc863d48b139044f42afca956e808545d340b
GitHub <noreply@github.com>
2026-08-03 10:45:07 +0000

    chore(update-schemas): 2026-08-03

--- internal/sdkprovider/userconfig/service/pg.go
+++ internal/sdkprovider/userconfig/service/pg.go
@@ -273,7 +273,7 @@ func pgUserConfig() *schema.Schema {
 						ValidateFunc: validation.StringInSlice([]string{"io_uring", "sync", "worker"}, false),
 					},
 					"io_workers": {
-						Description: "EXPERIMENTAL: Number of IO worker processes, for io_method=worker. Version 18 and up only. Default: `3`.",
+						Description: "EXPERIMENTAL: Number of IO worker processes, for io_method=worker. Version 18 and up only.",
 						Optional:    true,
 						Type:        schema.TypeInt,
 					},
