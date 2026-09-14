9d83e458dd9769f508a4fb86e5c45f262e7c7bb0
Stacey Salamon <stacey.salamon@aiven.io>
2026-09-14 17:28:40 +0200

    docs(Kafka): note that classic Kafka plans are shown in Console

--- internal/schemautil/service.go
+++ internal/schemautil/service.go
@@ -187,7 +187,7 @@ func ServiceCommonSchema() map[string]*schema.Schema {
 					return nil, nil
 				},
 			),
-			Description: "Defines what kind of computing resources are allocated for the service. Plan names must be lowercase alphanumeric (e.g., `business-8`, `my_plan_16`). It can be changed after creation, though there are some restrictions when going to a smaller plan such as the new plan must have sufficient amount of disk space to store all current data and switching to a plan with fewer nodes might not be supported. The basic plan names are `hobbyist`, `startup-x`, `business-x` and `premium-x` where `x` is (roughly) the amount of memory on each node (also other attributes like number of CPUs and amount of disk space varies but naming is based on memory). The available options can be seen from the [Aiven pricing page](https://aiven.io/pricing).",
+			Description: "Plans define the computing resources allocated for the service. The plan names lowercase alphanumeric values in the format `TIER-MEMORY`, for example: `business-8` or `startup-32`. The available options can be seen on the [Aiven pricing page](https://aiven.io/pricing). Kafka Classic plan names are shown in the [Aiven Console](https://console.aiven.io). When changing to a smaller plan, the new plan must have enough disk space to store all current data. Switching to a plan with fewer nodes is not supported for all services.",
 		},
 		"service_name": {
 			Type:        schema.TypeString,
